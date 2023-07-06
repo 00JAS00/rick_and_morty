ee9c07cf169746d0a8f81eaf93f6ed507f075a86
Julian Andres Sosa <julian.m.sosa.muchut@gmail.com>
2023-07-06 19:42:16 -0300

    ok

---
+++ Server/src/DB_connection.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const { DB_USER, DB_PASSWORD, DB_HOST,DB_NAME } = process.env;
+const { Sequelize } = require('sequelize');
+const FavoriteModel=require('./models/FavoriteModel');
+const UserModel=require('./models/UserModel');
+
+
+
+
+const sequelize = new Sequelize(
+   // URL
+   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+   
+   { logging: false, native: false }
+);
+
+
+FavoriteModel(sequelize);
+UserModel(sequelize);
+
+const { User, Favorite } = sequelize.models;
+User.belongsToMany(Favorite, {through:'user_favorite'});
+Favorite.belongsToMany(User, {through:'user_favorite'})
+
+module.exports = {
+   User,
+   Favorite,
+   conn: sequelize,
+};
--- Server/src/index.js
+++ Server/src/index.js
@@ -3,10 +3,14 @@ const express = require('express');
 const server = express();
 const PORT = 3001;
 const {router}=require('./routes/index')
+const {conn}=require('./DB_connection')
 
-server.listen(PORT, () => {
+
+server.listen(PORT, async () => {
+   await conn.sync({force: true})
    console.log('Server OK in port: ' + PORT);
 });
+
 server.use((req, res, next) => {
    res.header('Access-Control-Allow-Origin', '*');
    res.header('Access-Control-Allow-Credentials', 'true');
@@ -20,5 +24,7 @@ server.use((req, res, next) => {
    );
    next();
 });
+
 server.use(express.json())
+
 server.use('/rickandmorty',router)
